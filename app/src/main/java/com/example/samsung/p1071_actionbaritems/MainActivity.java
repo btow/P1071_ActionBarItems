a162ee264d550b2aa1b7149fb32ba68167ad7f68
btow <btow@yandex.ru>
2017-05-04 18:25:03 +0500

    Some experimentation with ActionBar

--- app/src/main/java/com/example/samsung/p1071_actionbaritems/MainActivity.java
+++ app/src/main/java/com/example/samsung/p1071_actionbaritems/MainActivity.java
@@ -1,20 +1,46 @@
 package com.example.samsung.p1071_actionbaritems;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActionBar ab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+//        myToolbar.setLogo(R.mipmap.ic_launcher_round);
+//        setSupportActionBar(myToolbar);
+
+        // Get a support ActionBar corresponding to this toolbar
+        ab = getSupportActionBar();
+        //Enable the Home button
+//        ab.setHomeButtonEnabled(true);
+        // Enable the Up button
+        ab.setDisplayHomeAsUpEnabled(true);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == android.R.id.home) {
+            ab.setTitle("New title");
+            ab.setLogo(R.mipmap.ic_launcher_round);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
